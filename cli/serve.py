459f1be3b0b0bdf96a1fbd15e6ff53bd693afe45
Gavin Staniforth <gsdevme@users.noreply.github.com>
2023-03-12 20:36:31 +0000

    style: correct line returns

--- cli/serve.py
+++ cli/serve.py
@@ -1,4 +1,5 @@
 import uvicorn
 
+
 def serve():
     uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=False, log_level="debug", workers=1, limit_concurrency=3)
--- cli/set_charge.py
+++ cli/set_charge.py
@@ -1,7 +1,7 @@
 from solis.inverter import Inverter
 from solis.modbus import Modbus
 
+
 def set_charge(amps: float):
     inverter = Inverter(Modbus())
     inverter.set_grid_charging_amps(amps)
-
